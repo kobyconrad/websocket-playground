0359193d9632608264263793561251b8c1c79741
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-07-29 14:21:03 -0700

    built basic client

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const client = net.createConnection({ port: 9898 }, () => {
+  console.log("CLIENT: I connected to the server.");
+  client.write("CLIENT: Hello this is client!");
+});
+
+client.on("data", (data) => {
+  console.log(data.toString());
+  client.end();
+});
+
+client.on("end", () => {
+  console.log("CLIENT: I disconnected from the server");
+});
